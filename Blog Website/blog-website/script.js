2b092b33b8f9d1f86844ffd453997afbce9b46c0
SONU-RAI-7 <sonuroy1818@gmail.com>
2025-01-25 15:15:56 +0530

    Add files via upload

---
+++ Blog Website/blog-website/script.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+document.getElementById('join-form').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  console.log(`Name: ${name}, Email: ${email}, Password: ${password}`);
+
+  document.getElementById('join-form').reset();
+});
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  this.classList.toggle("active");
+}
+
+addEventOnElem(navToggler, "click", toggleNavbar);
+
+const closeNavbar = function (event) {
+  navbar.classList.remove("active");
+  navToggler.classList.remove("active");
+
+  const targetId = this.getAttribute("href");
+  const targetSection = document.querySelector(targetId);
+  if (targetSection) {
+    event.preventDefault(); 
+    targetSection.scrollIntoView({ behavior: "smooth" });
+  }
+}
+
+addEventOnElem(navbarLinks, "click", closeNavbar);
+
+const searchBar = document.querySelector("[data-search-bar]");
+const searchTogglers = document.querySelectorAll("[data-search-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleSearchBar = function () {
+  searchBar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("active");
+}
+
+addEventOnElem(searchTogglers, "click", toggleSearchBar);
